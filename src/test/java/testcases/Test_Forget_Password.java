002e936de1e88ec09303a0aba45f2ead0a959075
Fransiskusandika <dikakoko@icloud.com>
2020-06-23 13:31:41 +0700

    Fix testcases

---
+++ src/test/java/testcases/Test_Forget_Password.java
@@ -0,0 +1,43 @@
+package testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import config.Setup;
+import config.Url;
+import object.Alert;
+import object.Button;
+import object.Input;
+import utility.ReadExcel;
+
+public class Test_Forget_Password extends Setup {
+	
+	public String path = "../pwa_ui_test/src/main/java/data/data_test.xlsx";
+	
+	@DataProvider
+	public String[][] forget_password_alert() throws InvalidFormatException, IOException {
+		ReadExcel read = new ReadExcel();
+		return read.getCellData(path, "Forget_Password_Alert");
+	}
+	
+	@Test(priority = 0, testName = "Forget Password - Alert ", dataProvider = "forget_password_alert")
+	public void forget_password_alert(String username, String alertText) {
+		Url url = new Url(driver);
+		Button button = PageFactory.initElements(driver, Button.class);
+		Input input = PageFactory.initElements(driver, Input.class);
+		Alert alert = PageFactory.initElements(driver, Alert.class);
+		
+		System.out.println("Forget Password - Alert");
+		
+		url.urls("/forget-password");
+		input.byId("input-email-phone", username);
+		button.byId("button-next");
+		alert.byId("invalid-feedback", alertText);
+	}
+	
+}
